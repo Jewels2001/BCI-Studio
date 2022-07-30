05db751f115370ce6ea55bd62dcce5dce7946b64
riccirich <88801679+riccirich@users.noreply.github.com>
2022-07-30 15:14:19 -0600

    vue web app

---
+++ natweb/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="logo" src="./assets/minion.jpg">
+  <HelloWorld msg="BCI Wizards: Brain Musical Visualizer!!!!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
