b37d1d4e2539fe56d1cf1d7882d0c2a63ebb8159
riccirich <88801679+riccirich@users.noreply.github.com>
2022-07-30 16:39:09 -0600

    added router

--- natweb/src/App.vue
+++ natweb/src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <img alt="logo" src="./assets/minion.jpg">
-  <HomePage msg="BCI Wizards: Brain Musical Visualizer!!!!"/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HomePage from './components/HomePage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HomePage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
