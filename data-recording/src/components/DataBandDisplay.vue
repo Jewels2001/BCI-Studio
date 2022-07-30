c1eed323775ff2af38bc87826aa28662048f9a79
dustin-ward <wardddustin@gmail.com>
2022-07-30 00:52:44 -0600

    reading data from muse

---
+++ data-recording/src/components/DataBandDisplay.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  ch: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div v-for="(label, i) in ch.labels">
+        <h3>{{label}}</h3>
+        <p>{{ch.datasets[0].data[i]}}</p>
+    </div>
+</template>
