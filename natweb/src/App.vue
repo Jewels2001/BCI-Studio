53de8fd7ea574ad97da774cade486125063334fc
riccirich <dagta.io@gmail.com>
2022-07-31 00:43:11 -0600

    more front-end work

--- natweb/src/App.vue
+++ natweb/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="logo" src="./assets/minion.jpg">
-  <HomePage msg="BCI Wizards: Brain Musical Visualizer!!!!"/>
+  <!-- <img alt="logo" src="./assets/minion.jpg"> -->
+  <!-- <HomePage msg="BCI Wizards: Brain Musical Visualizer!!!!"/> -->
+  <SideBar/>
+  <router-view/>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+// import HomePage from './components/HomePage.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage
-  }
+    SideBar
 }
+}
+
 </script>
 
 <style>
--- natweb/src/views/HomeView.vue
+++ natweb/src/views/HomeView.vue
@@ -1,10 +1,3 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HomePage msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
 import HomePage from '@/components/HomePage.vue'
