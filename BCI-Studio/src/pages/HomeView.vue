85cc86040086a5df09a2619ab062a52f88d9bcf0
Jewels2001 <juliewq25@gmail.com>
2022-07-31 17:07:46 -0600

    Feature: Temporary nav bar and routes

--- BCI-Studio/src/pages/HomeView.vue
+++ BCI-Studio/src/pages/HomeView.vue
@@ -9,3 +9,16 @@ export default {
   }
 }
 </script>
+
+<template>
+  <img alt="logo" src="../assets/minion.jpg">
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      This app uses your very own brain data to create SICK visuals and music. <br>
+    </p>
+    <button v-on:click="alert('hi!')" >
+    Click here to start
+    </button>
+  </div>
+</template>
